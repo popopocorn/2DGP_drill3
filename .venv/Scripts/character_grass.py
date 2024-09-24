8d30798b93ca1ba2210fe1871b393d02e6c698fa
popopocorn <popcorn375@naver.com>
2024-09-24 09:49:11 +0900

    pico

--- .venv/Scripts/character_grass.py
+++ .venv/Scripts/character_grass.py
@@ -1,7 +1,21 @@
 from pico2d import *
 
 open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
 
-# fill here
+def run_rectangle():
+    pass
+def run_circle():
+    pass
 
+
+
+
+while True:
+
+    run_rectangle()
+
+    run_circle()
+    
 close_canvas()
