cb1b4ee34a00e39879f7ad7daf592e5186359727
popopocorn <popcorn375@naver.com>
2024-09-24 10:02:21 +0900

    캐릭터 출력

--- .venv/Scripts/character_grass.py
+++ .venv/Scripts/character_grass.py
@@ -5,12 +5,14 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 def run_rectangle():
+    clear_canvas_now()
     print('rectangle')
     delay(0.5)
 def run_circle():
-    dx=0
-    dy=0
-    
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(400, 90)
+
 
 
 
@@ -27,5 +29,5 @@ while True:
     run_rectangle()
 
     run_circle()
-
+    break
 close_canvas()
